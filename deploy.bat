ce499985b70d9b5fd45521fdbcc6891c6a0ba485
JewelleWilson <74629827+SunRises-On@users.noreply.github.com>
2022-12-14 15:52:11 -0600

    Update and rename deploy.bat to deploy.java

--- deploy.bat
+++
@@ -1,2 +0,0 @@
-
-echo "Deploy passed"
---
+++ deploy.java
@@ -0,0 +1,9 @@
+class deploy(
+  
+    public static void main (String[] args){
+      
+      System.out.println("Deployed successfully!"); 
+      
+    }
+  
+  }
